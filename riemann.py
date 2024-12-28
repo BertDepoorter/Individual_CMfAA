11fbfd9af8c25a31d91c6a3556e9d8f596d3eabf
BertDepoorter <117076861+BertDepoorter@users.noreply.github.com>
2024-12-28 22:05:19 +0100

    create numerical schemes function

--- riemann.py
+++ riemann.py
@@ -36,7 +36,7 @@ class riemann:
         self.rho_L = rho_L
         self.rho_R = rho_R
     
-    def hugoniiot_locus(self, U, rho_range):
+    def hugoniot_locus(self, U, rho_range):
         '''
         function that calculates the hugoniot curves in state space for a given state U = (rho, m)
 
@@ -58,6 +58,60 @@ class riemann:
         m_neg = rho/rho_hat*m_hat - np.sqrt(rho/rho_hat)*ci*(rho-rho_hat)
 
         return np.array([m_pos, m_neg])
+    
+    def integral_curve(self, U, rho_range, wave_type):
+        '''
+        Compute integral curves (rarefactions) for given state U
 
+        input:
+        - U (2-tuple): contains initial state
+        - rho_range (array): values for which to calculate rarefaction
+        - wave_type (str): specify wave type
+            options include '1-rarefaction', '2-rarefaction'
 
+        output:
+        - array of (rho, v) values
+        '''
+        rho_hat, v_hat = U
 
+        # check input type
+        if (wave_type != '1-rarefaction') and (wave_type != '2-rarefaction'):
+            raise ValueError("Invalid wave type. Choose '1-rarefaction' or '2-rarefaction'.")
+
+        # proceed to calculation 
+        if wave_type == '1-rarefaction':
+            v = v_hat - c_i * np.log(rho / rho_hat)
+        elif wave_type == '2-rarefaction':
+            v = v_hat + c_i * np.log(rho / rho_hat)
+        return v
+
+    def solve_riemann_problem(self, method, limiter, timestepper):
+        '''
+        Function that solves the 1D Riemann problem numerically. 
+
+        input: 
+        - method (str): which flux discretization method to use
+            Options: 'tvdlf', 'upwind', 'maccormack'
+        - limiter (str): which limiter for the flux reconstruction to use. 
+            Options: 'minmod', 'woodward'
+        - timestepper (str): which numerical scheme to use for the numerical time integraton of fluxes
+            Options: 'onestep', 'twostep'
+        '''
+        methods = ['tvdlf', 'upwind', 'maccormack']
+        if method not in methods:
+            raise ValueError(("Invalid flux scheme. Choose 'tvdlf', 'upwind' or 'maccormack'."))
+        
+        timesteppers = ['onestep', 'twostep']
+        if timestepper not in timesteppers:
+            raise ValueError(("Invalid time stepper. Choose 'onestep' or 'twostep'."))
+        
+        limiters = ['minmod', 'woodward']
+        if limiter not in limiters:
+            raise ValueError("Invalid limiter. Choose from 'minmod' or 'woodward'.")
+        
+        # here mke sure the right subfunctions are called
+
+    def _TVDLF_flux_step(self, U_range):
+        '''
+        
+        '''
